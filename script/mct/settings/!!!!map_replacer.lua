f020ee0a29e8a740e360db8cf7db361981fed394
Frodo45127 <frodo_gv@hotmail.com>
2023-05-15 18:25:19 +0200

    Added nuln buildings.

---
+++ script/mct/settings/!!!!map_replacer.lua
@@ -0,0 +1,9 @@
+--[[-------------------------------------------------------------------------------------------------------------
+    Script by Frodo45127 for the Map Replacer & Assets mod.
+
+    MCT Module, with support MCT 0.9.
+]]---------------------------------------------------------------------------------------------------------------
+
+--local loc_prefix = "mct_map_rep_";
+--local mct = get_mct()
+--local mod = mct:register_mod("map_replacer");
